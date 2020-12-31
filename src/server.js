89c2427e13870a377e3deafbb74beb9f432eb851
kbbushman <k.bushman@yahoo.com>
2020-12-30 19:55:03 -0800

    Adds express body parser

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const User = require('./user/User');
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/api/v1/users', (req, res) => {
   User.create(req.body).then(() => {
     return res.send({ message: 'User created successfully' });
