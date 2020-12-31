c8371eb2d594ea5002c53bf406332d44f7644e2a
kbbushman <k.bushman@yahoo.com>
2020-12-30 21:20:05 -0800

    Refactor adds createUser function

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -12,82 +12,55 @@ beforeEach(() => {
 });
 
 describe('User Registration', () => {
+  const createUser = () => {
+    return request(app).post('/api/v1/users').send({
+      username: 'test1',
+      email: 'test@test.com',
+      password: '1234',
+    });
+  };
+
   it('returns 200 on successful signup request', (done) => {
-    request(app)
-      .post('/api/v1/users')
-      .send({
-        username: 'test1',
-        email: 'test@test.com',
-        password: '1234',
-      })
-      .then((response) => {
-        expect(response.status).toBe(200);
-        done();
-      });
+    createUser().then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
   });
 
   it('returns a success message on successful signup request', (done) => {
-    request(app)
-      .post('/api/v1/users')
-      .send({
-        username: 'test1',
-        email: 'test@test.com',
-        password: '1234',
-      })
-      .then((response) => {
-        expect(response.body.message).toBe('User created successfully');
-        done();
-      });
+    createUser().then((response) => {
+      expect(response.body.message).toBe('User created successfully');
+      done();
+    });
   });
 
   it('saves the user to the database', (done) => {
-    request(app)
-      .post('/api/v1/users')
-      .send({
-        username: 'test1',
-        email: 'test@test.com',
-        password: '1234',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          expect(userList.length).toBe(1);
-          done();
-        });
+    createUser().then(() => {
+      User.findAll().then((userList) => {
+        expect(userList.length).toBe(1);
+        done();
       });
+    });
   });
 
   it('saves the username and email to the database', (done) => {
-    request(app)
-      .post('/api/v1/users')
-      .send({
-        username: 'test1',
-        email: 'test@test.com',
-        password: '1234',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          const savedUser = userList[0];
-          expect(savedUser.username).toBe('test1');
-          expect(savedUser.email).toBe('test@test.com');
-          done();
-        });
+    createUser().then(() => {
+      User.findAll().then((userList) => {
+        const savedUser = userList[0];
+        expect(savedUser.username).toBe('test1');
+        expect(savedUser.email).toBe('test@test.com');
+        done();
       });
+    });
   });
 
   it('saves password hash to the database', (done) => {
-    request(app)
-      .post('/api/v1/users')
-      .send({
-        username: 'test1',
-        email: 'test@test.com',
-        password: '1234',
-      })
-      .then(() => {
-        User.findAll().then((userList) => {
-          const savedUser = userList[0];
-          expect(savedUser.password).not.toBe('1234');
-          done();
-        });
+    createUser().then(() => {
+      User.findAll().then((userList) => {
+        const savedUser = userList[0];
+        expect(savedUser.password).not.toBe('1234');
+        done();
       });
+    });
   });
 });
