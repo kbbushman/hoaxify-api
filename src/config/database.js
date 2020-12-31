84cc48a52d0e1f29d7f3440ce942569999cf1034
kbbushman <k.bushman@yahoo.com>
2020-12-30 20:02:18 -0800

    Adds logging false to sequelize config, commented out for now

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('hoaxify', 'db-user', 'dp-pass', {
   dialect: 'sqlite',
   storage: './database.sqlite',
+  // logging: false, // Add to prevent query logs in jest output
 });
 
 module.exports = sequelize;
