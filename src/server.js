825ba0140dfbb746e8aacfa50ebaf09138c1586b
kbbushman <k.bushman@yahoo.com>
2020-12-30 20:46:45 -0800

    Adds bcrypt to create user controller for password hash

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('./user/User');
 
 const app = express();
@@ -6,8 +7,12 @@ const app = express();
 app.use(express.json());
 
 app.post('/api/v1/users', (req, res) => {
-  User.create(req.body).then(() => {
-    return res.send({ message: 'User created successfully' });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = { ...req.body, password: hash };
+
+    User.create(user).then(() => {
+      return res.send({ message: 'User created successfully' });
+    });
   });
 });
 
