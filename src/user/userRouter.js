8abc6473759a1647bb2f44def13ecd279cb2dc75
kbbushman <k.bushman@yahoo.com>
2020-12-31 14:39:03 -0800

    Adds username validation as router middleware function

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -3,6 +3,15 @@ const userController = require('./userController');
 
 const router = express.Router();
 
-router.post('/', userController.create);
+const validateUsername = (req, res, next) => {
+  if (req.body.username === null) {
+    return res
+      .status(400)
+      .json({ validationErrors: { username: 'Username cannot be null' } });
+  }
+  next();
+};
+
+router.post('/', validateUsername, userController.create);
 
 module.exports = router;
