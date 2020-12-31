d5367f14af3520e7e377d2a04be786c420356989
kbbushman <k.bushman@yahoo.com>
2020-12-30 19:36:56 -0800

    Adds database save test to UserRegistration spec

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,5 +1,11 @@
 const request = require('supertest');
 const app = require('../src/server');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  return sequelize.sync();
+});
 
 describe('User Registration', () => {
   it('returns 200 on successful signup request', (done) => {
@@ -27,6 +33,22 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created successfully');
         done();
+      });
+  });
+
+  it('saves the user to the database', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'test1',
+        email: 'test@test.com',
+        password: '1234',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
       });
   });
 });
