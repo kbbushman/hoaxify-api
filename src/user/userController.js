0df84c77107f601ddd82bdc93bea34cc5690c20f
kbbushman <k.bushman@yahoo.com>
2020-12-30 21:12:29 -0800

    Refactor moves create in export to create function

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcrypt');
 const User = require('./User');
 
+const create = async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const user = { ...req.body, password: hash };
+  await User.create(user);
+  return res.send({ message: 'User created successfully' });
+};
+
 module.exports = {
-  create: async (req, res) => {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    const user = { ...req.body, password: hash };
-    await User.create(user);
-    return res.send({ message: 'User created successfully' });
-  },
+  create,
 };
