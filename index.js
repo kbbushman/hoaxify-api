01e8a37d7587929542e0a7eeeb35e73a32b5ec92
kbbushman <k.bushman@yahoo.com>
2020-12-30 20:28:22 -0800

    Adds sequlize db connection

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const app = require('./src/server');
+const sequelize = require('./src/config/database');
 const PORT = process.env.PORT || 4000;
+
+sequelize.sync();
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
