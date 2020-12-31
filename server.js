5cc02de74ea922c07ea821121d9425a955f8b982
kbbushman <k.bushman@yahoo.com>
2020-12-30 17:20:48 -0800

    Adds route and controller to handle initial user registration test

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.post('/api/v1/users', (req, res) => {
+  return res.send({ message: 'User created successfully' });
+});
+
 module.exports = app;
