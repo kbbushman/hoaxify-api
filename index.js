c4281a38a1381ec474146aa88753b0b2622c7906
kbbushman <k.bushman@yahoo.com>
2020-12-30 17:26:30 -0800

    Updates server.js import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./server');
+const app = require('./src/server');
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
