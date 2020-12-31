c6aa6edc1d10802abd55d351c7c00556f5c1ae3a
kbbushman <k.bushman@yahoo.com>
2020-12-30 23:14:13 -0800

    Adds cross-env database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-const sequelize = new Sequelize('hoaxify', 'db-user', 'dp-pass', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  // logging: false, // Add to prevent query logs in jest output
-});
+const dbConfig = config.get('database');
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage,
+    logging: dbConfig.logging, // false to prevent query logs in jest output
+  }
+);
 
 module.exports = sequelize;
