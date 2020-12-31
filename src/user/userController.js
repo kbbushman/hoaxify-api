2d8f47460ce56c12bacc9f98d36615b12e63df92
kbbushman <k.bushman@yahoo.com>
2020-12-31 13:05:22 -0800

    Adds 400 response to create userController if username is null

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 
 const create = async (req, res) => {
+  if (req.body.username === null) {
+    return res.sendStatus(400);
+  }
   const hash = await bcrypt.hash(req.body.password, 10);
   const user = { ...req.body, password: hash };
   await User.create(user);
