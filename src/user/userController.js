6938901a141d31810d170c7115ba1d1ac786c07f
kbbushman <k.bushman@yahoo.com>
2020-12-31 14:38:38 -0800

    Refactor moves username validation to router middleware function

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,11 +2,6 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 
 const create = async (req, res) => {
-  if (req.body.username === null) {
-    return res
-      .status(400)
-      .json({ validationErrors: { username: 'Username cannot be null' } });
-  }
   const hash = await bcrypt.hash(req.body.password, 10);
   const user = { ...req.body, password: hash };
   await User.create(user);
