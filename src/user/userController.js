bca76124b8a2fac075dfacffc874457e8c45fad4
kbbushman <k.bushman@yahoo.com>
2021-01-01 14:32:36 -0800

    Refactors nodemailer into emailService

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,8 +1,7 @@
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
-const nodemailer = require('nodemailer');
-const nodemailerStub = require('nodemailer-stub');
 const User = require('./User');
+const emailService = require('../email/emailService');
 
 const generateToken = (length) => {
   return crypto.randomBytes(length).toString('hex').substring(0, length);
@@ -19,13 +18,7 @@ const create = async (req, res) => {
   };
   await User.create(user);
 
-  const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
-  await transporter.sendMail({
-    from: 'My App <info@any-app.com>',
-    to: email,
-    subject: 'Account Activation',
-    html: `Token is ${user.activationToken}`,
-  });
+  await emailService.sendAccountActivation(email, user.activationToken);
 
   return res.send({ message: req.t('user_create_success') });
 };
