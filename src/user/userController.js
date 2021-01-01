62f1764916e792e95452059493c88c1a3698006e
kbbushman <k.bushman@yahoo.com>
2021-01-01 00:18:20 -0800

    Removes try/catch for dupliate email, handled in router

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,16 +2,10 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 
 const create = async (req, res) => {
-  try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    const user = { ...req.body, password: hash };
-    await User.create(user);
-    return res.send({ message: 'User created successfully' });
-  } catch (err) {
-    return res
-      .status(400)
-      .send({ validationErrors: { email: 'Email already in use' } });
-  }
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const user = { ...req.body, password: hash };
+  await User.create(user);
+  return res.send({ message: 'User created successfully' });
 };
 
 module.exports = {
