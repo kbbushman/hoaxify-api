72ba40e5e11a6a93cdaa50f632f36558d24e1e7f
kbbushman <k.bushman@yahoo.com>
2021-01-01 02:31:50 -0800

    Adds i18next translation to user create success message

--- src/user/userController.js
+++ src/user/userController.js
@@ -5,7 +5,7 @@ const create = async (req, res) => {
   const hash = await bcrypt.hash(req.body.password, 10);
   const user = { ...req.body, password: hash };
   await User.create(user);
-  return res.send({ message: 'User created successfully' });
+  return res.send({ message: req.t('user_create_success') });
 };
 
 const findByEmail = async (email) => {
