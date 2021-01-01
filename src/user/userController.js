cf746b59908c75275414699bb31f94013fd2736b
kbbushman <k.bushman@yahoo.com>
2021-01-01 13:31:45 -0800

    Adds req body destructuring to prevent inactive property override

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,8 +2,9 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 
 const create = async (req, res) => {
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hash };
+  const { username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = { username, email, password: hash };
   await User.create(user);
   return res.send({ message: req.t('user_create_success') });
 };
