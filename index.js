8b2dd72cefb68205cc39755a303774a52eb387c0
kbbushman <k.bushman@yahoo.com>
2020-12-31 23:45:18 -0800

    Adds config object with force set to true to sync database with latest updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./src/server');
 const sequelize = require('./src/config/database');
 const PORT = process.env.PORT || 4000;
 
-sequelize.sync();
+// {force: true} sync database with latest updates
+sequelize.sync({ force: true });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
