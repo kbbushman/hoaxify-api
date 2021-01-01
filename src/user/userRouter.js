f366a13519c8c047e10a569d0f30c20b8230ae02
kbbushman <k.bushman@yahoo.com>
2020-12-31 20:05:11 -0800

    Adds username length validation

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -18,7 +18,12 @@ const validateRequest = (req, res, next) => {
 
 router.post(
   '/',
-  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4 })
+    .withMessage('Must have min 4 and max 32 characters'),
   check('email').notEmpty().withMessage('Email cannot be null'),
   check('password').notEmpty().withMessage('Password cannot be null'),
   validateRequest,
