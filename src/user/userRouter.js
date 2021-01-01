2a58f01c91a370b947eb967406ab039ce92b592d
kbbushman <k.bushman@yahoo.com>
2021-01-01 02:14:48 -0800

    Refactors for i18next validation message translations

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -10,7 +10,7 @@ const validateRequest = (req, res, next) => {
     const validationErrors = {};
     errors
       .array()
-      .forEach((error) => (validationErrors[error.param] = error.msg));
+      .forEach((error) => (validationErrors[error.param] = req.t(error.msg)));
     return res.status(400).send({ validationErrors });
   }
   next();
@@ -20,34 +20,32 @@ router.post(
   '/',
   check('username')
     .notEmpty()
-    .withMessage('Username cannot be null')
+    .withMessage('username_null')
     .bail()
     .isLength({ min: 4, max: 32 })
-    .withMessage('Must have min 4 and max 32 characters'),
+    .withMessage('username_length'),
   check('email')
     .notEmpty()
-    .withMessage('Email cannot be null')
+    .withMessage('email_null')
     .bail()
     .isEmail()
-    .withMessage('Email is not valid')
+    .withMessage('email_invalid')
     .bail()
     .custom(async (email) => {
       const user = await userController.findByEmail(email);
       if (user) {
-        throw new Error('Email already in use');
+        throw new Error('email_inuse');
       }
     }),
   check('password')
     .notEmpty()
-    .withMessage('Password cannot be null')
+    .withMessage('password_null')
     .bail()
     .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters')
+    .withMessage('password_length')
     .bail()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-    .withMessage(
-      'Password must have at least 1 lowercase letter, 1 uppercase letter, and 1 number'
-    ),
+    .withMessage('password_pattern'),
   validateRequest,
   userController.create
 );
