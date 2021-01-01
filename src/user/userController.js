c1cf290162feb271e708256194cec410f455810b
kbbushman <k.bushman@yahoo.com>
2020-12-31 23:56:22 -0800

    Adds try/catch block in create method to catch sequelize email validation error

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,10 +2,16 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 
 const create = async (req, res) => {
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hash };
-  await User.create(user);
-  return res.send({ message: 'User created successfully' });
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = { ...req.body, password: hash };
+    await User.create(user);
+    return res.send({ message: 'User created successfully' });
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ validationErrors: { email: 'Email already in use' } });
+  }
 };
 
 module.exports = {
