7037323cb93cef061ec61e80115b54a2609d5f3b
kbbushman <k.bushman@yahoo.com>
2020-12-31 17:40:56 -0800

    Adds password validation to create route in userRouter

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -20,6 +20,7 @@ router.post(
   '/',
   check('username').notEmpty().withMessage('Username cannot be null'),
   check('email').notEmpty().withMessage('Email cannot be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
   validateRequest,
   userController.create
 );
