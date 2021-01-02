d27f32a3a25a16b075b929f23760b1b4ba503d38
kbbushman <k.bushman@yahoo.com>
2021-01-02 12:58:45 -0800

    Updates account activation email message, console logs email url in development

--- src/email/emailService.js
+++ src/email/emailService.js
@@ -1,12 +1,29 @@
+const nodemailer = require('nodemailer');
 const transporter = require('../config/emailTransporter');
 
 const sendAccountActivation = async (email, token) => {
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: 'My App <info@any-app.com>',
     to: email,
     subject: 'Account Activation',
-    html: `Token is ${token}`,
+    html: `
+      <div>
+        <p>
+          <strong>Please click the link below to activate your account</strong>
+        </p>
+      </div>
+      <div>
+        <p>
+          <a href="http://localhost:8080/#/login?token=${token}">Activate Account</a>
+        </p>
+      </div>
+    `,
   });
+  if (process.env.NODE_ENV === 'development') {
+    console.log(
+      `ACCOUNT ACTIVATION URL: ${nodemailer.getTestMessageUrl(info)}`
+    );
+  }
 };
 
 module.exports = {
