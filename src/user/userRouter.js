857f090de65e1ef2bd67c3a72fe4c36f6e40a47e
kbbushman <k.bushman@yahoo.com>
2021-01-02 14:20:28 -0800

    Removes unused code

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -8,11 +8,6 @@ const router = express.Router();
 const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    // const validationErrors = {};
-    // errors
-    //   .array()
-    //   .forEach((error) => (validationErrors[error.param] = req.t(error.msg)));
-    // return res.status(400).send({ validationErrors });
     return next(new ValidationException(errors.array()));
   }
   next();
