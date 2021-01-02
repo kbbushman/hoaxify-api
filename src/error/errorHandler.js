f3b683ba2b170206b0af30fcbe33e4eb7d185196
kbbushman <k.bushman@yahoo.com>
2021-01-02 15:17:14 -0800

    Adds timestamp to error response object

--- src/error/errorHandler.js
+++ src/error/errorHandler.js
@@ -11,7 +11,7 @@ module.exports = (err, req, res, next) => {
   }
   res.status(status).send({
     path: req.originalUrl,
-    timestamp: '',
+    timestamp: new Date().getTime(),
     message: req.t(message),
     validationErrors,
   });
