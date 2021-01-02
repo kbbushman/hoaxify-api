eab6dc965dd88a4fceb570621b0d0cbabd9fbb49
kbbushman <k.bushman@yahoo.com>
2021-01-02 14:19:56 -0800

    Updates error handler middlware for ValidationException errors from router validatRequest

--- src/server.js
+++ src/server.js
@@ -30,8 +30,16 @@ app.use('/api/v1/users', userRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  const { status, message } = err;
-  res.status(status).send({ message: req.t(message) });
+  const { status, message, errors } = err;
+  let validationErrors;
+  // Errors from express-validator
+  if (errors) {
+    validationErrors = {};
+    errors.forEach(
+      (error) => (validationErrors[error.param] = req.t(error.msg))
+    );
+  }
+  res.status(status).send({ message: req.t(message), validationErrors });
 });
 
 module.exports = app;
