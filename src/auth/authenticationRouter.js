1dc3db29e186db5ab1d60c6314c9ed45e84b04be
kbbushman <k.bushman@yahoo.com>
2021-01-03 14:12:01 -0800

    Adds authenticationRouter with post placeholder

---
+++ src/auth/authenticationRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// const { check, validationResult } = require('express-validator');
+// const User = require('../user/User');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  res.send();
+});
+
+module.exports = router;
