82555dfef8b216f4a4d1ca06c1f9ee3d565f4a7f
kbbushman <k.bushman@yahoo.com>
2021-01-02 16:19:13 -0800

    Adds getUusers content length test

--- __tests__/UserListing.spec.js
+++ __tests__/UserListing.spec.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
 const app = require('../src/server');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('Listing Users', () => {
   it('returns 200 ok when there are no users in the database', async () => {
@@ -15,5 +25,16 @@ describe('Listing Users', () => {
       size: 10,
       totalPages: 0,
     });
+  });
+
+  it('returns 10 users in page content when there are 11 users in the database', async () => {
+    for (let i = 0; i < 11; i++) {
+      await User.create({
+        username: `user${i + 1}`,
+        email: `user${i + 1}@test.com`,
+      });
+    }
+    const response = await request(app).get('/api/v1/users');
+    expect(response.body.content.length).toBe(10);
   });
 });
