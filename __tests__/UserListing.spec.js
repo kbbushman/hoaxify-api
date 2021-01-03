e397077ba48cee48cd0b03da3aebf2b7efd1df85
kbbushman <k.bushman@yahoo.com>
2021-01-02 16:24:57 -0800

    Refactors tests with getUsers function

--- __tests__/UserListing.spec.js
+++ __tests__/UserListing.spec.js
@@ -17,12 +17,12 @@ const getUsers = () => {
 
 describe('Listing Users', () => {
   it('returns 200 ok when there are no users in the database', async () => {
-    const response = await request(app).get('/api/v1/users');
+    const response = await getUsers();
     expect(response.status).toBe(200);
   });
 
   it('returns page object as response body', async () => {
-    const response = await request(app).get('/api/v1/users');
+    const response = await getUsers();
     expect(response.body).toEqual({
       content: [],
       page: 0,
@@ -38,7 +38,7 @@ describe('Listing Users', () => {
         email: `user${i + 1}@test.com`,
       });
     }
-    const response = await request(app).get('/api/v1/users');
+    const response = await getUsers();
     expect(response.body.content.length).toBe(10);
   });
 });
