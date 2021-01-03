b062d6d70b21dd9a3cb98a348c5d3fe4259d9099
kbbushman <k.bushman@yahoo.com>
2021-01-02 16:27:31 -0800

    Adds addUsers function, refactors page content length test

--- __tests__/UserListing.spec.js
+++ __tests__/UserListing.spec.js
@@ -15,6 +15,15 @@ const getUsers = () => {
   return request(app).get('/api/v1/users');
 };
 
+const addUsers = async (count) => {
+  for (let i = 0; i < count; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@test.com`,
+    });
+  }
+};
+
 describe('Listing Users', () => {
   it('returns 200 ok when there are no users in the database', async () => {
     const response = await getUsers();
@@ -32,12 +41,7 @@ describe('Listing Users', () => {
   });
 
   it('returns 10 users in page content when there are 11 users in the database', async () => {
-    for (let i = 0; i < 11; i++) {
-      await User.create({
-        username: `user${i + 1}`,
-        email: `user${i + 1}@test.com`,
-      });
-    }
+    await addUsers(11);
     const response = await getUsers();
     expect(response.body.content.length).toBe(10);
   });
