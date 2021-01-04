7c9ac27c99e258e26f17bb32c21d1f657c7cd466
kbbushman <k.bushman@yahoo.com>
2021-01-04 00:54:30 -0800

    Updates login route with express-validator email check

--- src/auth/authenticationRouter.js
+++ src/auth/authenticationRouter.js
@@ -1,9 +1,23 @@
 const express = require('express');
-// const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const authenticationController = require('./authenticationController');
+const AuthenticationException = require('./AuthenticationException');
+
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new AuthenticationException());
+  }
+  next();
+};
 
 const router = express.Router();
 
-router.post('/', authenticationController.login);
+router.post(
+  '/',
+  check('email').isEmail(),
+  validateRequest,
+  authenticationController.login
+);
 
 module.exports = router;
