453ff21588d38427169706ea65f0a7426e642170
kbbushman <k.bushman@yahoo.com>
2021-01-05 22:07:56 -0800

    Refactors beforeAll with async/await

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -31,9 +31,9 @@ beforeAll(async () => {
   await sequelize.sync();
 });
 
-beforeEach(() => {
+beforeEach(async () => {
   simulateSmtpFailure = false;
-  return User.destroy({ truncate: true });
+  await User.destroy({ truncate: true });
 });
 
 afterAll(async () => {
