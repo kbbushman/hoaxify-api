4b1ef9f1f7f731b6fc8bba4f7ccf2af7b1e0c56b
kbbushman <k.bushman@yahoo.com>
2021-01-06 23:38:07 -0800

    Adds hashed passwords to temp users

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const app = require('./src/server');
 const sequelize = require('./src/config/database');
+const bcrypt = require('bcrypt');
 const User = require('./src/user/User');
 const PORT = process.env.PORT || 4000;
 
 const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  const hash = await bcrypt.hash('P4ssword', 10);
   for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
     await User.create({
       username: `user${i + 1}`,
       email: `user${i + 1}@test.com`,
       inactive: i >= activeUserCount,
+      password: hash,
     });
   }
 };
