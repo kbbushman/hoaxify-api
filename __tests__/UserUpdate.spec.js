2c07a6906944aebb512fe65f829e699d0aaf2589
kbbushman <k.bushman@yahoo.com>
2021-01-06 23:53:33 -0800

    Adds 403 on unauthenticated user update test

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const app = require('../src/server');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
-const en = require('../locales/en/translation.json');
-const es = require('../locales/es/translation.json');
+// const en = require('../locales/en/translation.json');
+// const es = require('../locales/es/translation.json');
 
 beforeAll(async () => {
   await sequelize.sync();
@@ -12,4 +12,11 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   await User.destroy({ truncate: true });
+});
+
+describe('User Update', () => {
+  it('returns forbidden when request sent without basic authorization', async () => {
+    const response = await request(app).put('/api/v1/users/5').send();
+    expect(response.status).toBe(403);
+  });
 });
