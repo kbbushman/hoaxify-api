c396aca04ee0a45ec01dbc0f2ad9f49f037d98d5
kbbushman <k.bushman@yahoo.com>
2021-01-07 23:35:59 -0800

    Adds error body response test for unauthd user update

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -3,8 +3,8 @@ const request = require('supertest');
 const app = require('../src/server');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
-// const en = require('../locales/en/translation.json');
-// const es = require('../locales/es/translation.json');
+const en = require('../locales/en/translation.json');
+const es = require('../locales/es/translation.json');
 
 beforeAll(async () => {
   await sequelize.sync();
@@ -19,4 +19,22 @@ describe('User Update', () => {
     const response = await request(app).put('/api/v1/users/5').send();
     expect(response.status).toBe(403);
   });
+
+  it.each`
+    language | message
+    ${'es'}  | ${es.unauthorized_user_update}
+    ${'en'}  | ${en.unauthorized_user_update}
+  `(
+    'returns error body with $message for unauthorized request when language is set to $language',
+    async ({ language, message }) => {
+      const nowInMillis = new Date().getTime();
+      const response = await request(app)
+        .put('/api/v1/users/5')
+        .set('Accept-Language', language)
+        .send();
+      expect(response.body.path).toBe('/api/v1/users/5');
+      expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
+      expect(response.body.message).toBe(message);
+    }
+  );
 });
