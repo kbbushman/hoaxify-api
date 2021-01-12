50f4ccd101dd1c769afa1e1f101427168fab9482
kbbushman <k.bushman@yahoo.com>
2021-01-11 23:44:05 -0800

    Updates login success response body test with check for token

--- __tests__/Auth.spec.js
+++ __tests__/Auth.spec.js
@@ -45,7 +45,7 @@ describe('Authentication', () => {
     expect(response.status).toBe(200);
   });
 
-  it('returns only user id and username when login success', async () => {
+  it('returns only user id, username and token when login success', async () => {
     const user = await addUser();
     const response = await postAuthentication({
       email: 'test@test.com',
@@ -53,7 +53,7 @@ describe('Authentication', () => {
     });
     expect(response.body.id).toBe(user.id);
     expect(response.body.username).toBe(user.username);
-    expect(Object.keys(response.body)).toEqual(['id', 'username']);
+    expect(Object.keys(response.body)).toEqual(['id', 'username', 'token']);
   });
 
   it('returns 401 when user does not exist', async () => {
