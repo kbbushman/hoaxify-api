32828ea350ffc31b4d041cabcb9f67952209898d
kbbushman <k.bushman@yahoo.com>
2021-01-13 23:42:44 -0800

    Adds verify for token verification

--- src/auth/tokenService.js
+++ src/auth/tokenService.js
@@ -4,6 +4,11 @@ const createToken = (user) => {
   return jwt.sign({ id: user.id }, 'temporary-secret');
 };
 
+const verify = (token) => {
+  return jwt.verify(token, 'temporary-secret');
+};
+
 module.exports = {
   createToken,
+  verify,
 };
