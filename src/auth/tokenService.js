e2f26741145ae90e4215c00814e79f9b98f48c27
kbbushman <k.bushman@yahoo.com>
2021-01-17 23:20:41 -0800

    Refactors oneWeekAgo with global const

--- src/auth/tokenService.js
+++ src/auth/tokenService.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 const Token = require('./Token');
 const { randomString } = require('../shared/generator');
 
+const ONE_WEEK_IN_MILLIS = 7 * 24 * 60 * 60 * 1000;
+
 const createToken = async (user) => {
   const token = randomString(32);
   await Token.create({
@@ -13,7 +15,7 @@ const createToken = async (user) => {
 };
 
 const verify = async (token) => {
-  const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+  const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
   const tokenInDB = await Token.findOne({
     where: {
       token,
@@ -33,7 +35,7 @@ const deleteToken = async (token) => {
 };
 
 const scheduleCleanup = async () => {
-  const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+  const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
   await Token.destroy({
     where: {
       lastUsedAt: {
