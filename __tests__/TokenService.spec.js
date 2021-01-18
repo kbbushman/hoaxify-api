f2015e2d56571a7730c0cdefefae7e1ba8441a10
kbbushman <k.bushman@yahoo.com>
2021-01-17 22:51:30 -0800

    Adds TokenService spec with scheduled cleanup test

---
+++ __tests__/TokenService.spec.js
@@ -0,0 +1,25 @@
+const sequelize = require('../src/config/database');
+const Token = require('../src/auth/Token');
+const tokenService = require('../src/auth/tokenService');
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+
+beforeEach(async () => {
+  await Token.destroy({ truncate: true });
+});
+
+describe('Scheduled Token Cleanup', () => {
+  it('clears the expired token with scheduled task', async () => {
+    const token = 'test-token';
+    const eightDaysAgo = new Date(Date.now() - 8 * 24 * 60 * 60 * 1000);
+    await Token.create({
+      token,
+      lastUsedAt: eightDaysAgo,
+    });
+    tokenService.scheduleCleanup();
+    const tokenInDB = await Token.findOne({ where: { token } });
+    expect(tokenInDB).toBeNull();
+  });
+});
