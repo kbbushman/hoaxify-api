7719e78f37c0cb88136ed6647d42b1aec2942839
kbbushman <k.bushman@yahoo.com>
2021-01-17 23:24:33 -0800

    Refactors scheduleCleanup with temp 1 sec interval

--- src/auth/tokenService.js
+++ src/auth/tokenService.js
@@ -34,15 +34,17 @@ const deleteToken = async (token) => {
   await Token.destroy({ where: { token } });
 };
 
-const scheduleCleanup = async () => {
-  const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
-  await Token.destroy({
-    where: {
-      lastUsedAt: {
-        [Sequelize.Op.lt]: oneWeekAgo,
+const scheduleCleanup = () => {
+  setInterval(async () => {
+    const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
+    await Token.destroy({
+      where: {
+        lastUsedAt: {
+          [Sequelize.Op.lt]: oneWeekAgo,
+        },
       },
-    },
-  });
+    });
+  }, 1000);
 };
 
 module.exports = {
