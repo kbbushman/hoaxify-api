d4aaac8b256ebbfc0154e958b1815b0fdc18b903
kbbushman <k.bushman@yahoo.com>
2021-01-20 23:52:29 -0800

    Adds sendPasswordReset

--- src/email/emailService.js
+++ src/email/emailService.js
@@ -26,6 +26,32 @@ const sendAccountActivation = async (email, token) => {
   }
 };
 
+const sendPasswordReset = async (email, token) => {
+  const info = await transporter.sendMail({
+    from: 'My App <info@any-app.com>',
+    to: email,
+    subject: 'Password Reset',
+    html: `
+      <div>
+        <p>
+          <strong>Please click the link below to reset your password</strong>
+        </p>
+      </div>
+      <div>
+        <p>
+          <a href="http://localhost:8080/#/password-reset?reset=${token}">Reset Password</a>
+        </p>
+      </div>
+    `,
+  });
+  if (process.env.NODE_ENV === 'development') {
+    console.log(
+      `ACCOUNT ACTIVATION URL: ${nodemailer.getTestMessageUrl(info)}`
+    );
+  }
+};
+
 module.exports = {
   sendAccountActivation,
+  sendPasswordReset,
 };
