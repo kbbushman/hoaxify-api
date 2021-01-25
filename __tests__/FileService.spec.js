edff79ffb67c6982774501ae13687bca88af9a97
kbbushman <k.bushman@yahoo.com>
2021-01-24 21:24:19 -0800

    Adds creates profile directory under upload directory test

--- __tests__/FileService.spec.js
+++ __tests__/FileService.spec.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const fileService = require('../src/file/fileService');
 
 describe('Create Folders', () => {
@@ -6,5 +7,11 @@ describe('Create Folders', () => {
     fileService.createFolders();
     const folderName = 'upload';
     expect(fs.existsSync(folderName)).toBe(true);
+  });
+
+  it('creates profile folder under upload folder', async () => {
+    fileService.createFolders();
+    const profileFolder = path.join('.', 'upload', 'profile');
+    expect(fs.existsSync(profileFolder)).toBe(true);
   });
 });
