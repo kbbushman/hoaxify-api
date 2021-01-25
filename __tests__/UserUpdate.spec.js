4b2fcdfd711bff2ea82107e53f1fe4cb6270fece
kbbushman <k.bushman@yahoo.com>
2021-01-24 23:42:32 -0800

    Adds jest afterAll to cleanup image uploads

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -17,6 +17,15 @@ beforeEach(async () => {
   await User.destroy({ truncate: { cascade: true } });
 });
 
+afterAll(() => {
+  const { uploadDir, profileDir } = config;
+  const profileDirectory = path.join('.', uploadDir, profileDir);
+  const files = fs.readdirSync(profileDirectory);
+  for (const file of files) {
+    fs.unlinkSync(path.join(profileDirectory, file));
+  }
+});
+
 const activeUser = {
   username: 'test1',
   email: 'test@test.com',
