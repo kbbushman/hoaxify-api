733699d201e8fd9be86811251ca8a2981caa2371
kbbushman <k.bushman@yahoo.com>
2021-01-24 21:27:02 -0800

    Adds conditional profile directory

--- src/file/fileService.js
+++ src/file/fileService.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
 const createFolders = () => {
   const uploadDir = 'upload';
+  const profileFolder = path.join('.', 'upload', 'profile');
   if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync('upload');
+    fs.mkdirSync(uploadDir);
+  }
+  if (!fs.existsSync(profileFolder)) {
+    fs.mkdirSync(profileFolder);
   }
 };
 
