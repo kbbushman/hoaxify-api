7ba59f6fc6cd14e04b94962c6896245834cf3fd3
kbbushman <k.bushman@yahoo.com>
2021-01-24 21:46:55 -0800

    Refactors tests with directory names from config

--- __tests__/FileService.spec.js
+++ __tests__/FileService.spec.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const fileService = require('../src/file/fileService');
+const config = require('config');
+const { uploadDir, profileDir } = config;
 
 describe('Create Folders', () => {
   it('creates an upload folder', async () => {
     fileService.createFolders();
-    const folderName = 'upload';
-    expect(fs.existsSync(folderName)).toBe(true);
+    expect(fs.existsSync(uploadDir)).toBe(true);
   });
 
   it('creates profile folder under upload folder', async () => {
     fileService.createFolders();
-    const profileFolder = path.join('.', 'upload', 'profile');
+    const profileFolder = path.join('.', uploadDir, profileDir);
     expect(fs.existsSync(profileFolder)).toBe(true);
   });
 });
