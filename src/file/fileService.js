d707ae8affbf8358de6749f5e874410c48123b28
kbbushman <k.bushman@yahoo.com>
2021-01-24 21:44:09 -0800

    Refactor adds upload and profile directory names from config

--- src/file/fileService.js
+++ src/file/fileService.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const config = require('config');
 
 const createFolders = () => {
-  const uploadDir = 'upload';
-  const profileFolder = path.join('.', 'upload', 'profile');
+  const { uploadDir, profileDir } = config;
+  const profileFolder = path.join('.', uploadDir, profileDir);
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
