f649c06a2c9edfee27b18f8107329a37f25b0e7a
kbbushman <k.bushman@yahoo.com>
2021-01-26 18:38:59 -0800

    Adds images route and express.static for profile images

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const express = require('express');
 const i18next = require('i18next');
 const Backend = require('i18next-fs-backend');
 const middleware = require('i18next-http-middleware');
+const config = require('config');
 const userRouter = require('./user/userRouter');
 const authenticationRouter = require('./auth/authenticationRouter');
 const errorHandler = require('./error/errorHandler');
 const tokenAuthentication = require('./middleware/tokenAuthentication');
 const fileService = require('./file/fileService');
+
+const { uploadDir, profileDir } = config;
+const profileFolder = path.join('.', uploadDir, profileDir);
 
 i18next
   .use(Backend)
@@ -30,6 +35,8 @@ const app = express();
 
 app.use(middleware.handle(i18next));
 app.use(express.json());
+
+app.use('/images', express.static(profileFolder));
 
 app.use(tokenAuthentication);
 
