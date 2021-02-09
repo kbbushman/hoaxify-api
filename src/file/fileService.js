59b972eea90cd62ae05f1afb3434dee0ea2fb447
kbbushman <k.bushman@yahoo.com>
2021-02-08 22:13:52 -0800

    Adds isLessThan2MB and isSupportedFileType methods

--- src/file/fileService.js
+++ src/file/fileService.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const config = require('config');
+const FileType = require('file-type');
 const { randomString } = require('../shared/generator');
 
 const { uploadDir, profileDir } = config;
@@ -27,8 +28,21 @@ const deleteProfileImage = async (filename) => {
   await fs.promises.unlink(filePath);
 };
 
+const isLessThan2MB = (buffer) => {
+  return buffer.length < 2 * 1024 * 1024;
+};
+
+const isSupportedFileType = async (buffer) => {
+  const type = await FileType.fromBuffer(buffer);
+  return !type
+    ? false
+    : type.mime === 'image/png' || type.mime === 'image/jpeg';
+};
+
 module.exports = {
   createFolders,
   saveProfileImage,
   deleteProfileImage,
+  isLessThan2MB,
+  isSupportedFileType,
 };
